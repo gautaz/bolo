87d4f1d1d7e809bb2090eae2fb1b901176093c33
Thibault Hild <gautaz@users.noreply.github.com>
2016-04-03 22:48:06 +0200

    node 0.x compatibility

--- example/monoprocess.js
+++ example/monoprocess.js
@@ -1,10 +1,11 @@
+// keep this file compatible with node 0.x
 'use strict'
 
 var Promise = require('pinkie-promise')
 var inspect = require('util').inspect
 var bolo = require('../index')
 
-var wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+var wait = function (ms) { return new Promise(function (resolve) { setTimeout(resolve, ms) }) }
 
 var logger = function (prefix) {
   return {
@@ -17,27 +18,27 @@ var logger = function (prefix) {
 
 var storeA, storeB
 
-bolo({ log: logger('A') }).then((bolo) => {
+bolo({ log: logger('A') }).then(function (bolo) {
   storeA = bolo
-  storeA.on('set', (key, datum) => console.log(`A just received ${inspect(datum)} for ${key}`))
-  storeA.on('remove', (key) => console.log(`${key} was removed from A`))
-  storeA.on('close', () => process.exit(0))
+  storeA.on('set', function (key, datum) { console.log('A just received', inspect(datum), 'for', key) })
+  storeA.on('remove', function (key) { console.log(key, 'was removed from A') })
+  storeA.on('close', function () { process.exit(0) })
   return wait(500)
-}).then(() => bolo({ log: logger('B') })).then((bolo) => {
+}).then(function () { return bolo({ log: logger('B') }) }).then(function (bolo) {
   storeB = bolo
-  storeB.on('set', (key, datum) => console.log(`B just received ${inspect(datum)} for ${key}`))
-  storeB.on('remove', (key) => console.log(`${key} was removed from B`))
-  storeB.on('close', () => storeA.close())
+  storeB.on('set', function (key, datum) { console.log('B just received', inspect(datum), 'for', key) })
+  storeB.on('remove', function (key) { console.log(key, 'was removed from B') })
+  storeB.on('close', function () { storeA.close() })
   return wait(500)
-}).then(() => {
+}).then(function () {
   storeA.set('test', { test: true })
   return wait(500)
-}).then(() => {
+}).then(function () {
   storeA.close()
   return wait(500)
-}).then(() => {
+}).then(function () {
   storeB.close()
-}).catch((error) => {
+}).catch(function (error) {
   console.log(error.stack ? error.stack : error)
   process.exit(1)
 })
